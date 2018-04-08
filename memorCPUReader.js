685f2bc92763ac5f07cdf86fc131f6037befe310
ankitm27 <anki.malhotra00@gmail.com>
2018-04-08 20:02:47 +0530

    monitoring script added

---
+++ memorCPUReader.js
@@ -0,0 +1,35 @@
+var cron = require('cron');
+const os = require('os-utils');
+const { exec } = require('child_process');
+const dotenv = require('dotenv');
+dotenv.load();
+
+let memoryUsage = os.freememPercentage() * 100;
+let cpuUsage;
+
+
+let triggerPythonScript = (type, cpuUsage, memoryUsage) => {
+    exec('python notification.py ' +  type  + " "  + cpuUsage + " " + memoryUsage, (err, stdout, stderr) => {
+        //console.log("err",err);
+        //console.log("stdout",stdout);
+        //console.log("stderr",stderr);
+    });
+}
+
+var job = new cron.CronJob('* * * * *', function () {
+    os.cpuUsage(function (cpuUsage) {
+        cpuUsage = cpuUsage * 100;
+        if (memoryUsage > process.env.MEMORY_USAGE && cpuUsage > process.env.cpu_USAGE) {
+            triggerPythonScript("both", memoryUsage, cpuUsage);
+        }
+        else if (memoryUsage > process.env.MEMORY_USAGE) {
+            triggerPythonScript("memory", memoryUsage);
+        }
+        else if (cpuUsage > process.env.CPU_USAGE) {
+            triggerPythonScript("cpu", cpuUsage);
+        }
+        //else {
+        //    console.log("every thing fine");
+        //}
+    });
+},null,true);
---
+++ notification.py
@@ -0,0 +1,35 @@
+import sys
+import smtplib
+subject = "Alert usage more then threshold"
+to_addr_list = "ankit.malhotra2506@gmail.com"
+USERNAME = ''
+PASSWORD = ''
+
+
+def mail(to_addr_list, subject, message, cc_addr_list=[], from_add=USERNAME, username=USERNAME, password=PASSWORD):
+    print "check";
+    message = "From: {from_addr}\nTo: {to_addr}\nCc: {cc}\nSubject: {subject}\n\n{message}".format(
+        from_addr = from_add,
+        to_addr = ', '.join(to_addr_list),
+        cc = ', '.join(cc_addr_list),
+        subject = subject,
+        message = message
+    )
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+    server.login(username, password)
+    problems = server.sendmail(from_add, to_addr_list, message)
+    server.quit()
+    print problems
+
+def main():
+    if sys.argv[1] == "both":
+        msg = "Memory usage is more then threshold, Currently memory usage is " + sys.argv[2] + ". and  CPU usage is also more then threshold Currentely, Currentely Usage is " + sys.argv[2] + "."
+    elif sys.argv[1] == "memory":
+        msg = "Memory usage is more then threshold, Currently memory usage is " + sys.argv[2] + "."
+    else:
+        msg = "CPU usage is more then threshold Currently, Currently Usage is " + sys.argv[2] + "."
+    mail(to_addr_list,subject,msg);
+
+
+main()

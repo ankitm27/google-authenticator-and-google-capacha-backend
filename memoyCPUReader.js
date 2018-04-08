3bf76c379520a11845b4dcf38f08a4396297af9d
ankitm27 <anki.malhotra00@gmail.com>
2018-04-08 22:56:51 +0530

    memory reader file comment changes

--- memoyCPUReader.js
+++ memoyCPUReader.js
@@ -10,17 +10,13 @@ let cpuUsage;
 
 let triggerPythonScript = (type, cpuUsage, memoryUsage) => {
     exec('python notification.py ' +  type  + " "  + cpuUsage + " " + memoryUsage, (err, stdout, stderr) => {
-        //console.log("err",err);
-        //console.log("stdout",stdout);
-        //console.log("stderr",stderr);
     });
-    console.log("check")
 }
 
 var job = new cron.CronJob('* * * * *', function () {
     os.cpuUsage(function (cpuUsage) {
         cpuUsage = cpuUsage * 100;
-        if (memoryUsage > process.env.MEMORY_USAGE && cpuUsage > process.env.cpu_USAGE) {
+        if (memoryUsage > process.env.MEMORY_USAGE && cpuUsage > process.env.CPU_USAGE) {
             triggerPythonScript("both", memoryUsage, cpuUsage);
         }
         else if (memoryUsage > process.env.MEMORY_USAGE) {
@@ -29,8 +25,5 @@ var job = new cron.CronJob('* * * * *', function () {
         else if (cpuUsage > process.env.CPU_USAGE) {
             triggerPythonScript("cpu", cpuUsage);
         }
-        //else {
-        //    console.log("every thing fine");
-        //}
     });
 },null,true);

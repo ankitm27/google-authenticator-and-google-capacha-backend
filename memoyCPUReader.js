86764a502a34188db3956596c9dfe1dd8314e5a1
ankitm27 <anki.malhotra00@gmail.com>
2018-04-08 22:38:38 +0530

    memory reader file change

--- memoyCPUReader.js
+++ memoyCPUReader.js
@@ -14,6 +14,7 @@ let triggerPythonScript = (type, cpuUsage, memoryUsage) => {
         //console.log("stdout",stdout);
         //console.log("stderr",stderr);
     });
+    console.log("check")
 }
 
 var job = new cron.CronJob('* * * * *', function () {
